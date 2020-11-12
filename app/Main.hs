90edade71452ae415eaa1a77cea370a32b9ec317
raptazure <raptazure@foxmail.com>
2020-11-12 11:39:26 +0800

    ?

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+import Control.Concurrent.STM
+import Data.Map
 import Lib
+import Network.Socket
 
 main :: IO ()
-main = someFunc
+main = withSocketsDo $ do
+  database <- atomically $ newTVar $ fromList [("__version__", "0.1.0")]
+  sock <- listen 7777
+  sockHandler sock database
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,82 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  (
+  )
+where
+
+import Control.Concurrent.STM (TVar, atomically, modifyTVar, readTVar)
+import Data.Attoparsec.ByteString (count)
+import Data.Attoparsec.ByteString.Char8
+  ( Parser,
+    char,
+    choice,
+    decimal,
+    endOfLine,
+    signed,
+    take,
+  )
+import Data.ByteString.Char8 (ByteString)
+import Data.Map (Map)
+import Network.Socket
+import Prelude hiding (lookup, take)
+
+type Value = ByteString
+
+type Key = ByteString
+
+type DB = Map Key Value
+
+data Command
+  = Get Key
+  | Set Key Value
+  | Unkown
+  deriving (Eq, Show)
+
+data Reply
+  = Bulk (Maybe ByteString)
+  | MultiBulk (Maybe [Reply])
+  deriving (Eq, Show)
+
+parseReply :: Reply -> Maybe Command
+parseReply (MultiBulk (Just xs)) =
+  case xs of
+    [Bulk (Just "get"), Bulk (Just a)] -> Just $ Get a
+    [Bulk (Just "set"), Bulk (Just a), Bulk (Just b)] -> Just $ Set a b
+    _ -> Just Unkown
+parseReply _ = Nothing
+
+replyParser :: Parser Reply
+replyParser = choice [bulk, multiBulk]
+
+bulk :: Parser Reply
+bulk =
+  Bulk <$> do
+    len <- char '$' *> signed decimal <* endOfLine
+    if len < 0
+      then return Nothing
+      else Just <$> take len <* endOfLine
+
+multiBulk :: Parser Reply
+multiBulk =
+  MultiBulk <$> do
+    len <- char '*' *> signed decimal <* endOfLine
+    if len < 0
+      then return Nothing
+      else Just <$> count len replyParsert
+
+sockHandler :: Socket -> TVar DB -> IO ()
+sockHandler sock db = do
+  (handle, _, _) <- accept sock
+  hSetBuffering handle NoBuffering
+  hSetBinaryMode handle True
+  _ <- forkIO $ commandProcessor handle db
+  sockHandler sock db
+
+-- STM (software transactional memory) - change the value
+
+atomRead :: TVar a -> IO a
+atomRead = atomically . readTVar
+
+updateValue :: (DB -> DB) -> TVar DB -> IO ()
+updateValue fn x = atomically $ modifyTVar x fn
